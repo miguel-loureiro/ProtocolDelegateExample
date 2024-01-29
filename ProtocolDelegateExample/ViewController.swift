0708fcb71cb9472d59fedf930a1ba28e3459a5c5
Miguel Loureiro <miguel.silvaloureiro@gmail.com>
2024-01-29 08:48:59 +0000

    rename folder and add resources

---
+++ ProtocolDelegateExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProtocolDelegateExample
+//
+//  Created by António Loureiro on 29/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
