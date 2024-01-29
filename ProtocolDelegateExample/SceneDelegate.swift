5f1440b1c49a725883be3998ed72bbd11d7fbdbf
Miguel Loureiro <miguel.silvaloureiro@gmail.com>
2024-01-29 09:00:12 +0000

    update project initial files

--- ProtocolDelegateExample/SceneDelegate.swift
+++ ProtocolDelegateExample/SceneDelegate.swift
@@ -1,8 +1,9 @@
 //
 //  SceneDelegate.swift
-//  ProtocolDelegateExample
+//  ProtocolDemo
 //
-//  Created by António Loureiro on 29/01/2024.
+//  Created by Alex Nagy on 07/12/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
 //
 
 import UIKit
@@ -16,14 +17,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+//        guard let _ = (scene as? UIWindowScene) else { return }
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let rootViewController = ProfileViewController()
+            let navigationController = UINavigationController(rootViewController: rootViewController)
+            window.rootViewController = navigationController
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
